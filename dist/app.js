5b6c7a6724486e54680c228b65b07502d236a413
Littlebirdwarrior <camille.dorland@gmail.com>
2024-02-28 16:31:48 +0100

    video (v) 1 : création du fichier config

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+/*
+* Fonctiomment du typescript
+* le fichier src/app.ts compilé dans dist/app.js (lien dans le index.html)
+* car le .ts n'est pas intérprétable dans le navigateur
+* Création d'un fichier de config tsconfig.json pour permettre la compilation automatique
+* cmd terminal : # npx tsc --watch
+* */
+//Création du compteur JS
+// les lets sont convertis en var car les navigateur de destination sont un peu vieux
+//pour cibler un navigateur plus recent, parametrer en config config la "target"
+//pour ne pas parametrer les erreur "noEmitOnError"
+//pour stoper la conversion "noEmit" a true
+const compteur = document.querySelector('#compteur');
+let i = 0;
+const increment = (e) => {
+    i++;
+    //ici, 1er soucis de type : nombre dans une sting
+    compteur.querySelector('span').innerText = i.toString();
+};
+compteur.addEventListener('click', increment);
